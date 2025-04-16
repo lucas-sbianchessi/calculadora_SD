a3a14ba6478c7c48859f249d577582a17c942fab
Cleysso Schwanck <cleysso.s@edu.pucrs.br>
2025-04-16 08:42:55 -0300

    feat(calc): declarei modulo

---
+++ calc.sv
@@ -0,0 +1,11 @@
+module calculadora (
+    input logic [3] cmd,
+    input logic clock,
+    input logic reset,
+    
+    output logic [2] status,
+    output logic [4] data,
+    output logic [4] position
+);
+    
+endmodule
