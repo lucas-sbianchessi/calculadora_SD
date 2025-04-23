a5737b55e0f387c112c4c3bdbc7ce68cdb7228e3
Cleysso Schwanck <cleysso.s@edu.pucrs.br>
2025-04-23 06:27:50 -0300

    Revert "16 de abril"
    This reverts commit c2cf03e191cc41c49f0fbcbc8a9846477eb60175.

--- calc.sv
+++ calc.sv
@@ -7,26 +7,5 @@ module calculadora (
     output logic [4] data,
     output logic [4] position
 );
-    logic[32] data_reg;
-    logic    rest_ctrl;
-
-    control control(
-        .position(position),
-        .digit(data),
-        .clock(clock),
-        .reset(reset),
-    ); //acertar essa porra
     
-    //processo botar
-    casee(cmd)
-        4'b0000, 4'b0001, 4'b0010, 4'b0011, 4'b0100
-        4'b0101, 4'b0110, 4'b0111, 4'b1000, 4'b1001: begin
-        status = b'00;
-        data_reg = data_reg * 10 + cmd;
-        position = position + 1;
-
-        status = b'01;
-        end
-
-    end case; 
 endmodule
