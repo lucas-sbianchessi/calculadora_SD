209f94dee46cebab021be4875216fa7bfab1d270
Cleysso Schwanck <cleysso.s@edu.pucrs.br>
2025-04-23 06:16:02 -0300

    Revert "16 de abril"
    This reverts commit c2cf03e191cc41c49f0fbcbc8a9846477eb60175.

--- calc.sv
+++ calc.sv
@@ -7,28 +7,5 @@ module calculadora (
     output logic [3] data,
     output logic [3] position
 );
-    // comandos
-    localparam logic [3:0]
-        DIG_MAX   = 4'b1001,
-        CMD_PLUS  = 4'b1010,
-        CMD_MINUS = 4'b1011,
-        CMD_MUL   = 4'b1100,
-        CMD_RES   = 4'b1110,
-        CMD_CLR   = 4'b1111;
-
-    // estados
-    typedef enum logic [2:0] {IDLE, IN_A, OP, IN_B, COMPUTE, DONE, ERROR} state_t;
-    state_t state, next;
-
-    // registradores de operandos e resultado
-    logic [31:0] op_a, op_b, acc;
-    logic [31:0] mul_cnt;
-    logic [3:0]  op_sel;
-
-    // controle de quantos dígitos já recebidos (op_a/op_b)
-    logic [3:0] nA, nB;
-    logic       inB;
-
-    // saída
-    assign position = 3'd0;
+    
 endmodule
